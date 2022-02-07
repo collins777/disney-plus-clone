788f6fd1bc02ece7caae05b49c319cd53b9ccde1
Brandon Collins <bmc1793@icloud.com>
2022-02-06 22:06:38 -0500

    added header and home components, added project images and videos, updated global css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Nav>
+      <Logo src="" />
+      <NavMenu></NavMenu>
+    </Nav>
   );
 }
 
 export default Header;
+
+///////////////////////////////////////////////////////////////////////////
+//////  STYLED COMPONENTS
+
+const Nav = styled.nav`
+  height: 70px;
+  background-color: #090b13;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div``;
